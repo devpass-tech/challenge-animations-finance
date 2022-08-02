a454b7b6eb51a89ad3a0770358b5249612a27fca
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2022-08-02 20:01:37 -0300

    create lottie refresh control

--- solutions/devsprint-leonardo-santos-2/FinanceApp/Components/LottieRefreshControl/LottieRefreshControl.swift
+++ solutions/devsprint-leonardo-santos-2/FinanceApp/Components/LottieRefreshControl/LottieRefreshControl.swift
@@ -5,4 +5,44 @@
 //  Created by Alexandre Cardoso on 02/08/22.
 //
 
-import Foundation
+import Cartography
+import Lottie
+import UIKit
+
+final class LottieRefreshControl: UIRefreshControl {
+    
+    private let lottieName: String
+    private lazy var animationView: AnimationView = {
+        let animationView = AnimationView(name: lottieName)
+        animationView.loopMode = .loop
+        animationView.animationSpeed = 0.8
+        animationView.backgroundColor = .white
+        animationView.play()
+        return animationView
+    }()
+    
+    init(lottieName: String) {
+        self.lottieName = lottieName
+        super.init()
+        backgroundColor = .white
+        addAnimationLottie()
+        setupConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) { nil }
+    
+    private func addAnimationLottie() {
+        self.addSubview(animationView)
+    }
+    
+    private func setupConstraints() {
+        constrain(self, animationView) {
+            $1.top == $0.top
+            $1.leading == $0.leading
+            $1.trailing == $0.trailing
+            $1.bottom == $0.bottom
+        }
+    }
+
+}
