99778e1e559549b95ad33563c618f319dc14ca49
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-08-03 18:24:34 -0300

    Feature (Like): Add acessorio de view animado (#5)
    * Feature (Like): Add acessorio de view animado
    
    Descriçao:
    - Adicao do acessoryView
    - Adicao da animacao no fluxo normal 0% -> 100% para like
    - Adicao da aniamcao no fluxo reverso 100% -> 0% prara retirar-lo
    
    * Build: Resolve
    
    * Feature (AnimationView): AnimationViews Customizadas baseadas na biblioteca do lottie
    
    * Fix (Acessorio): Ajuste da interaçao usando o metodo touchBegan

--- solutions/devsprint-leonardo-santos-2/FinanceApp/Commons/Constants/LottieFile.swift
+++ solutions/devsprint-leonardo-santos-2/FinanceApp/Commons/Constants/LottieFile.swift
@@ -2,12 +2,15 @@
 //  LottieFile.swift
 //  FinanceApp
 //
-//  Created by Paolo Prodossimo Lopes on 28/07/22.
+//  Created by Paolo Prodossimo Lopes on 27/07/22.
 //
+
+import Foundation
 
 enum LottieFile: String {
     case splashScreen = "devpass_splashscreen"
     case circleLoading = "devpass_button_loading"
+    case like = "devpass_button_like"
     
     var file: String { rawValue }
 }
