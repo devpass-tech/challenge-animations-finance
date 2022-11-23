8addb6097c0b0851c98572f85a6b1b69900d1fbe
Leonardo Bandeira <leonardo.freire@usemobile.xyz>
2022-11-23 08:24:27 -0300

    Updated animation of PositioningViewController

--- solutions/devsprint-leonardo-santos-5/leonardo-bandeira/AnimationsShowcase/Showcases/FirstSection/Positioning.swift
+++ solutions/devsprint-leonardo-santos-5/leonardo-bandeira/AnimationsShowcase/Showcases/FirstSection/Positioning.swift
@@ -1,9 +1,47 @@
 import UIKit
 
 final class PositioningViewController: BaseViewController {
+    // MARK: - Property(ies).
+    private var pointPosition: Int = 0
+    private var points: [CGPoint] = [.init(x: 120, y: 0),
+                                     .init(x: 0, y: 0),
+                                     .init(x: 0, y: 120),
+                                     .init(x: 0, y: 0),
+                                     .init(x: -120, y: 0),
+                                     .init(x: 0, y: 0),
+                                     .init(x: 0, y: -120),
+                                     .init(x: 0, y: 0)]
+    private var timer: Timer?
+    
+    // MARK: - Override(s).
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @objc
     override func didTapActionButton() {
-        UIView.animate(withDuration: 1, delay: 0, options: [.autoreverse, .curveEaseInOut, .repeat]) {
-            self.square.transform = .init(translationX: 120, y: 60)
+        guard let _ = timer else {
+            self.timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                              target: self,
+                                              selector: #selector(didTapActionButton),
+                                              userInfo: nil,
+                                              repeats: true)
+            return
+        }
+
+        if pointPosition != points.endIndex {
+            animate(object: square, withTranslation: points[pointPosition])
+            pointPosition += 1
+        } else {
+            pointPosition = 0
+        }
+    }
+    
+    // MARK: - Method(s).
+    private func animate(object: UIView, withTranslation: CGPoint) {
+        UIView.animate(withDuration: 1.0, delay: 0, options: [.curveEaseInOut]) {
+            object.transform = .init(translationX: withTranslation.x, y: withTranslation.y)
         }
     }
 }
